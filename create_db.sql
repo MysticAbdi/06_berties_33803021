10bc17d63d3fb3ee0dd1e595fb519b4a1587ccea
MysticAbdi <mohabdikarin@gmail.com>
2025-11-20 21:34:28 +0000

    Added user and audit tables to DB setup script

--- create_db.sql
+++ create_db.sql
@@ -11,3 +11,21 @@ CREATE TABLE IF NOT EXISTS books (
     price  DECIMAL(5, 2),
     PRIMARY KEY(id));
 
+CREATE TABLE IF NOT EXISTS userData (
+    id             INT AUTO_INCREMENT,
+    username       VARCHAR(255) NOT NULL,
+    first_name     VARCHAR(255) NOT NULL,
+    last_name      VARCHAR(255) NOT NULL,
+    email          VARCHAR(255) NOT NULL,
+    hashedPassword VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS loginAttempts (
+    id          INT AUTO_INCREMENT,
+    username    VARCHAR(255) NOT NULL,
+    success     BOOLEAN      NOT NULL,
+    reason      VARCHAR(255) NOT NULL,
+    attemptTime DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
